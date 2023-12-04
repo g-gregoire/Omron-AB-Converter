f9799f5c605205acddee0fa06674f9ffef8c3a75
Georges Gregoire <66093809+g-gregoire@users.noreply.github.com>
2023-12-04 11:30:55 -0500

    minor updates/more files

--- file_functions.py
+++ file_functions.py
@@ -5,6 +5,7 @@ import components as cmp
 n = '\n'
 filename = "test.L5X"
 
+# Set directories
 dir = os.getcwd()
 ref_dir = os.path.join(dir, "Reference")
 output_dir = os.path.join(dir, "output")
@@ -23,3 +24,30 @@ def createfile(name="code", filetype="txt"):
     file = open(filename,"w") 
     return file
 
+# To be completed
+def addContext(file, phase):
+    pass
+
+# To be completed
+def addFooter(file):
+    pass
+
+# Specific routine logic
+def addRung(file, r_num, logic, comment=None):
+    text =cmp.r_start.replace("r_num", str(r_num)) + n
+    
+    # If comment, add comment
+    if comment != None:
+        text+=cmp.r_comment_start + comment +cmp.r_comment_end + n
+    
+    # Add Logic
+    text +=cmp.r_logic_start + logic +cmp.r_logic_end + n
+
+    # Add routine closure
+    text +=cmp.r_end
+    
+    file.write(text)
+    file.write(n)
+    r_num += 1
+
+    return file, r_num
