bab805f5e59fd8f9afd5c56314b33ef9b7ed328b
Georges Gregoire <66093809+g-gregoire@users.noreply.github.com>
2025-03-13 10:57:53 -0400

    updating instruction lookup
    - adding dict keys, add argument number

--- lookup.py
+++ lookup.py
@@ -7,173 +7,138 @@ routine_header = "routine-header.xml"
 footer = "footer.xml"
 tagcsv = "tag.csv"
 
-# Lookup conversion fOR Omron to AB
+# Lookup conversion for Omron to AB
 lookup = {
     # Comments
-    "'": "Comment",
-    # LOAD Instructions
-    "LD": ["LOAD", "XIC"],
-    "LDNOT": ["LOAD", "XIO"],
-    # END LOAD Instructions
-    "ORLD": ["LOAD", ""],
-    "ANDLD": ["LOAD", "]"],
-    # Input AND Instructions
-    "AND": ["AND", "XIC"],
-    "ANDNOT": ["AND", "XIO"],
-    # Input OR Instructions
-    "OR": ["OR", "XIC"],
-    "ORNOT": ["OR", "XIO"],
-    # Branching Instructions
-    # "STBR": ["BRANCH", "STBR"],
-    # "NWBR": ["BRANCH", "NWBR"],
+    "'": {"instr": "", "type": "comment", "args": 0},
+
+    # Load Instructions
+    "ANDLD": {"instr": "XIC", "type": "load", "args": 1},
+    "LD": {"instr": "XIC", "type": "load", "args": 1},
+    "LDNOT": {"instr": "XIO", "type": "load", "args": 1},
+    "ORLD": {"instr": "XIC", "type": "load", "args": 1},
+
+    # Logic Instructions
+    "AND": {"instr": "XIC", "type": "logic", "args": 1},
+    "ANDNOT": {"instr": "XIO", "type": "logic", "args": 1},
+    "ANDW(34)": {"instr": "AND", "type": "logic", "args": 1},
+    "OR": {"instr": "XIC", "type": "logic", "args": 1},
+    "ORNOT": {"instr": "XIO", "type": "logic", "args": 1},
 
     # Output Instructions
-    "OUT": ["OUTPUT", "OTE"],
-    "SET": ["OUTPUT", "OTL"],
-    "RSET": ["OUTPUT", "OTU"],
-    "DIFU(13)": ["oneshot", "OSR"],
-    "DIFD(14)": ["oneshot", "OSF"], 
-    "KEEP(11)": ["KEEP", "OTL"],
-    "OTU": ["KEEP", "OTU"],
+    "OUT": {"instr": "OTE", "type": "output", "args": 1},
+    "SET": {"instr": "OTL", "type": "output", "args": 1},
+    "RSET": {"instr": "OTU", "type": "output", "args": 1},
+    "KEEP(11)": {"instr": "OTL", "type": "output", "args": 1},
+    "OTU": {"instr": "OTU", "type": "output", "args": 1},
+
+    # Oneshot Instructions
+    "DIFD(14)": {"instr": "OSF", "type": "oneshot", "args": 1},
+    "DIFU(13)": {"instr": "OSR", "type": "oneshot", "args": 1},
 
     # Timer Instructions
-    "TIM": ["TIMER", "TON"],
-    # Count Instructions
-    "CNT": ["COUNTER", "CTU"],
-    "RESET": ["RESET", "RES"],
+    "TIM": {"instr": "TON", "type": "timer", "args": 1},
+    "TTIM(87)": {"instr": "RTO", "type": "timer", "args": 1},
+
+    # Counter Instructions
+    "CNT": {"instr": "CTU", "type": "counter", "args": 1},
+    "CNR(545)": {"instr": "RES", "type": "counter", "args": 1},
+
+    # Reset Instructions (Counter/Timer)
+    "RESET": {"instr": "RES", "type": "reset", "args": 1},
 
     # Comparison Instructions
-    "CMP(20)": ["COMPARE", ""],
-    "GREATER_THAN": ["COMPARE", "GRT"],
-    "P_GT": ["COMPARE", "GRT"],
-    "LESS_THAN": ["COMPARE", "LES"],
-    "P_LT": ["COMPARE", "LES"],
-    "EQUALS": ["COMPARE", "EQU"],
-    "P_EQ": ["COMPARE", "EQU"],
-    # Converted Comparison Instructions
-    "EQU": ["COMPARE", "EQU"],
-    "GRT": ["COMPARE", "GRT"],
-    "GEQ": ["COMPARE", "GEQ"],
-    "LES": ["COMPARE", "LES"],
-    "LEQ": ["COMPARE", "LEQ"],
+    "CMP(20)": {"instr": "", "type": "compare", "args": 2},
+    "CMPL(60)": {"instr": "", "type": "compare", "args": 2},
+    "AND<(310)": {"instr": "LT", "type": "compare", "args": 2},
+    "AND<=(315)": {"instr": "LE", "type": "compare", "args": 2},
+    "AND=(300)": {"instr": "EQ", "type": "compare", "args": 2},
+    "AND=L(300)": {"instr": "EQU", "type": "compare", "args": 2},
+    "AND>(320)": {"instr": "GT", "type": "compare", "args": 2},
+    "AND>=(325)": {"instr": "GE", "type": "compare", "args": 2},
+    "LD<(310)": {"instr": "LT", "type": "compare", "args": 2},
+    "LD<=(315)": {"instr": "LE", "type": "compare", "args": 2},
+    "LD=(300)": {"instr": "EQ", "type": "compare", "args": 2},
+    "LD>(320)": {"instr": "GT", "type": "compare", "args": 2},
+    "LD>=(325)": {"instr": "GE", "type": "compare", "args": 2},
+    "OR<=(315)": {"instr": "LEQ", "type": "or_compare", "args": 2},
+    "OR=(300)": {"instr": "EQU", "type": "or_compare", "args": 2},
+    "OR>(320)": {"instr": "GRT", "type": "or_compare", "args": 2},
+    "OR>=(325)": {"instr": "GEQ", "type": "or_compare", "args": 2},
+    "GREATER_THAN": {"instr": "GRT", "type": "compare", "args": 2},
+    "LESS_THAN": {"instr": "LES", "type": "compare", "args": 2},
+    "EQUALS": {"instr": "EQU", "type": "compare", "args": 2},
+    "P_GT": {"instr": "GRT", "type": "compare", "args": 2},
+    "P_LT": {"instr": "LES", "type": "compare", "args": 2},
+    "P_EQ": {"instr": "EQU", "type": "compare", "args": 2},
+    "EQU": {"instr": "EQU", "type": "compare", "args": 2},
+    "GRT": {"instr": "GRT", "type": "compare", "args": 2},
+    "GEQ": {"instr": "GEQ", "type": "compare", "args": 2},
+    "LES": {"instr": "LES", "type": "compare", "args": 2},
+    "LEQ": {"instr": "LEQ", "type": "compare", "args": 2},
 
     # Math Instructions
-    "ADD(30)": ["math", "ADD"],
-    "SUB(31)": ["math", "SUB"],
-    "MUL(32)": ["math", "MUL"],
-    "DIV(33)": ["math", "DIV"],
-    # Binary Math
-    "ADB(50)": ["math", "ADD"],
-    "SBB(51)": ["math", "SUB"],
-    "MLB(52)": ["math", "MUL"],
-    "DVB(53)": ["math", "DIV"],
-    # Binary Math
-    "ANDW(34)": ["math", "AND"],
-    
+    "-B(414)": {"instr": "SUB", "type": "math", "args": 3},
+    "*(420)": {"instr": "MUL", "type": "math", "args": 3},
+    "*B(424)": {"instr": "MUL", "type": "math", "args": 3},
+    "/(430)": {"instr": "DIV", "type": "math", "args": 3},
+    "/BL(435)": {"instr": "DIV", "type": "math", "args": 3},
+    "/UL(433)": {"instr": "DIV", "type": "math", "args": 3},
+    "+(400)": {"instr": "ADD", "type": "math", "args": 3},
+    "+B(404)": {"instr": "ADD", "type": "math", "args": 3},
+    "+BCL(407)": {"instr": "ADD", "type": "math", "args": 3},
+    "+C(402)": {"instr": "ADD", "type": "math", "args": 3},
+    "+CL(403)": {"instr": "ADD", "type": "math", "args": 3},
+    "+L(401)": {"instr": "ADD", "type": "math", "args": 3},
+    "ADD(30)": {"instr": "ADD", "type": "math", "args": 3},
+    "SUB(31)": {"instr": "SUB", "type": "math", "args": 3},
+    "MUL(32)": {"instr": "MUL", "type": "math", "args": 3},
+    "DIV(33)": {"instr": "DIV", "type": "math", "args": 3},
+    "ADB(50)": {"instr": "ADD", "type": "math", "args": 3},
+    "SBB(51)": {"instr": "SUB", "type": "math", "args": 3},
+    "MLB(52)": {"instr": "MUL", "type": "math", "args": 3},
+    "DVB(53)": {"instr": "DIV", "type": "math", "args": 3},
+
+    # Copy Instructions
+    "MOV(21)": {"instr": "MOV", "type": "copy", "args": 2},
+    "MOVD(83)": {"instr": "MOV", "type": "copy", "args": 2},
+    "MOVL(498)": {"instr": "MOV", "type": "copy", "args": 2},
+    "BCD(24)": {"instr": "MOV", "type": "copy", "args": 2},
+    "BCDL(59)": {"instr": "MOV", "type": "copy", "args": 2},
+    "BIN(23)": {"instr": "MOV", "type": "copy", "args": 2},
+    "XFER(70)": {"instr": "COP", "type": "copy", "args": 3},
+    "XFRB(62)": {"instr": "COP", "type": "copy", "args": 3},
+    "BSET(71)": {"instr": "FLL", "type": "copy", "args": 3}, # For fill length, Omron specifies start & end bits; AB specifies start bit & length
+    "MOVB(82)": {"instr": "BTD", "type": "copy", "args": 5}, # Omron has 3 args (Source, Control Word, Dest), AB has 5 (Source, Source bit, Dest, Dest Bit, Length).
+
     # Scaling Instructions
-    "SCL(64)": ["scaling", "CPT"],
+    "SCL(64)": {"instr": "CPT", "type": "scaling", "args": 2},
+    "SCL(194)": {"instr": "CPT", "type": "scaling", "args": 2},
+    "APR(69)": {"instr": "CPT", "type": "scaling", "args": 2},
 
     # PID Instructions
-    "PID(60)": ["pid", "PID"],
+    "PID(60)": {"instr": "PID", "type": "pid", "args": 3}, # PID Includes more than 3 args, but we're only interested in PID, PV and CV
+    "PID(190)": {"instr": "PID", "type": "pid", "args": 3}, # PID Includes more than 3 args, but we're only interested in PID, PV and CV
 
-    # Logical Instructions
-    "MOV(21)": ["logical", "MOV"],
-    "BIN(23)": ["logical", "MOV"],
-    "BCD(24)": ["logical", "MOV"],
-    "XFER(70)": ["copy", "COP"],
-    "MOVB(82)": ["btd", "BTD"],
+    # Stack Instructions
+    "PUSH(632)": {"instr": "FFL", "type": "stack", "args": 3}, # Args: Source, FIFO, Control Word. CW should be the same across the same FFL and FFU.
+    "FIFO(633)": {"instr": "FFU", "type": "stack", "args": 3}, # Args: FIFO, Dest, Control Word. CW should be the same across the same FFL and FFU.
 
-    # Other Instructions - No 1:1 conversion
-    # "CLC(41)": ["other", "CLC"],
-    # "BIN(23)": ["other", "BIN"], # Binary to BCD
-    # "BCD(24)": ["other", "BCD"], # BCD to Binary
-    # "ADB(50)": ["other", "ADB"],
-    # "SCL(64)": ["other", "SCL"],
-
-    # PID Instructions
-    # "PID(60)": ["pid", "PID"],
-
-    # End of rung
+    # End of Rung
     "^^^": ";\n",
 
-    "AND<(310)":	["LT",	"COMPARE"],
-    "AND<=(315)":	["LE",	"COMPARE"],
-    "AND=(300)":	["EQ",	"COMPARE"],
-    "AND=L(300)":	["EQU",	"COMPARE"],
-    "AND>(320)":	["GT",	"COMPARE"],
-    "AND>=(325)":	["GE",	"COMPARE"],
-    "CMP(20)":	["CMP",	"COMPARE"],
-    "CMPL(60)":	["CMP",	"COMPARE"],
-    "LD<(310)":	["LT",	"COMPARE"],
-    "LD<=(315)":	["LE",	"COMPARE"],
-    "LD=(300)":	["EQ",	"COMPARE"],
-    "LD>(320)":	["GT",	"COMPARE"],
-    "LD>=(325)":	["GE",	"COMPARE"],
-    "BCD(24)":	["TO_BCD",	"Copy"],
-    "BCDL(59)":	["MOV",	"Copy"],
-    "BIN(23)":	["BCD_TO",	"Copy"],
-    "BSET(71)":	["FLL",	"Copy"],
-    "MOV(21)":	["MOV",	"Copy"],
-    "MOVB(82)":	["MOVB",	"Copy"],
-    "MOVD(83)":	["MOV",	"Copy"],
-    "MOVL(498)":	["MOV",	"Copy"],
-    "XFER(70)":	["COP",	"Copy"],
-    "XFRB(62)":	["COP",	"Copy"],
-    "CNR(545)":	["RES",	"Counter"],
-    "CNT":	["CTU",	"Counter"],
-    "RESET":	["RES",	"Counter"],
-    "ANDLD":	["XIC",	"Load"],
-    "LD":	["XIC",	"Load"],
-    "LDNOT":	["XIO",	"Load"],
-    "ORLD":	["XIC",	"Load"],
-    "AND":	["XIC",	"Logic"],
-    "ANDNOT":	["XIO",	"Logic"],
-    "ANDW(34)":	["AND",	"Logic"],
-    "OR":	["XIC",	"Logic"],
-    "ORNOT":	["XIO",	"Logic"],
-    "PMCR(260)":["",	"Macro"],
-    "-B(414)":	["SUB",	"Math"],
-    "*(420)":	["MUL",	"Math"],
-    "*B(424)":	["MUL",	"Math"],
-    "/(430)":	["DIV",	"Math"],
-    "/BL(435)":	["DIV",	"Math"],
-    "/UL(433)":	["DIV",	"Math"],
-    "+(400)":	["ADD",	"Math"],
-    "+B(404)":	["ADD",	"Math"],
-    "+BCL(407)":	["ADD",	"Math"],
-    "+C(402)":	["ADD",	"Math"],
-    "+CL(403)":	["ADD",	"Math"],
-    "+L(401)":	["ADD",	"Math"],
-    "ADB(50)":	["ADD",	"Math"],
-    "ADD(30)":	["ADD",	"Math"],
-    "APR(69)":	["CPT",	"Math"],
-    "CADD(730)":	["?",	"Math"],
-    "DIV(33)":	["DIV",	"Math"],
-    "DVB(53)":	["DIV",	"Math"],
-    "MLB(52)":	["MUL",	"Math"],
-    "MUL(32)":	["MUL",	"Math"],
-    "SBB(51)":	["SUB",	"Math"],
-    "SUB(31)":	["SUB",	"Math"],
-    "DIFD(14)":	["OSF",	"oneshot"],
-    "DIFU(013)":	["OSR",	"oneshot"],
-    "DIFU(13)":	["OSR",	"oneshot"],
-    "OR<=(315)":	["LEQ*", "OR-COMPARE"],
-    "OR=(300)":	["EQU*", "OR-COMPARE"],
-    "OR>(320)":	["GRT*", "OR-COMPARE"],
-    "OR>=(325)":	["GEQ*", "OR-COMPARE"],
-    "KEEP(11)":	["OTL",	"Output"],
-    "OTU":	["OTU",	"Output"],
-    "OUT":	["OTE",	"Output"],
-    "RSET":	["OTU",	"Output"],
-    "SET":	["OTL",	"Output"],
-    "PID(190)":	["PID",	"PID"],
-    "PID(60)":	["PID",	"PID"],
-    "SCL(194)":	["SCL",	"Scaling"],
-    "SCL(64)":	["SCL",	"Scaling"],
-    "FIFO(633)":	["FFU",	"Stack"],
-    "PUSH(632)":	["FFL",	"Stack"],
-    "SSET(630)":	["?",	"Stack"],
-    "DATE(735)":	["?",	"System"],
-    "TIM":	["TON",	"Timer"],
-    "TTIM(87)":	["RTO",	"Timer"],
+     # Other Instructions - No 1:1 conversion
+    # "CLC(41)": ["other", "CLC"],
+    # "DATE(735)": ["?", "System"],
+    # "CADD(730)": ["?", "Math"],
+    # "PMCR(260)": {"instr": "", "type": "macro", "args": "?"},
+    # "SSET(630)": {"instr": "?", "type": "stack", "args": "?"}, # Not needed with ABR
+
+    # Ignored Instructions
+    "CLC(41)": {"instr": "ignore", "type": "macro", "args": "?"},
+    "DATE(735)": {"instr": "ignore", "type": "macro", "args": "?"},
+    "CADD(730)": {"instr": "ignore", "type": "macro", "args": "?"},
+    "PMCR(260)": {"instr": "ignore", "type": "macro", "args": "?"},
+    "SSET(630)": {"instr": "ignore", "type": "stack", "args": "?"}, # Not needed with ABR
+
 }
